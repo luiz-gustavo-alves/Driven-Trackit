e175f869e8d609199b0a9d9d03e2b9111a22e3c6
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-06-04 01:06:21 -0300

    Feat: react-calendar habits status

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,19 +1,120 @@
 /* Import Styled Components and Dependencies */
-import { PageContainer, PageContent, Container } from "./styled";
+import { PageContainer, PageContent, Container, CenterLoader } from "./styled";
+import { Oval } from 'react-loader-spinner';
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import dayjs from "dayjs";
+import axios from "axios";
+
+/* Local Imports */
+import BASE_URL from "../../constants/urls";
 
 export default function History() {
 
     const navigate = useNavigate();
 
-    if (localStorage.getItem("userData")) {
+    const [calendar, setCalendar] = useState(new Date());
+    const [calendarHabits, setCalendarHabits] = useState(null);
+    const [doneAllHabitsDays, setDoneAllHabitsDays] = useState(null);
+    const [notDoneAllHabitsDays, setNotDoneAllHabitsDays] = useState(null);
 
-        const { token } = JSON.parse(localStorage.getItem("userData"));
-        console.log(token);
+    useEffect(() => {
 
-    } else {
-        /* Unauthorized Access or localStorage data expired */
-        navigate("/");
+        if (localStorage.getItem("userData")) {
+
+            const checkCalendarHabitStatus = (calendarData) => {
+
+                const doneAllHabitsDays = [];
+                const notDoneAllHabitsDays = [];
+
+                for (let i = 0; i < calendarData.length; i++) {
+
+                    let allDayHabitsDone = true;
+                    const dayHabits = calendarData[i].habits;
+
+                    for (let j = 0; j < dayHabits.length; j++) {
+
+                        const habit = dayHabits[j];
+
+                        if (!habit.done) {
+                            allDayHabitsDone = false;
+                            break;
+                        }
+                    }
+
+                    const currentDay = calendarData[i].day;
+
+                    (allDayHabitsDone) ? doneAllHabitsDays.push(currentDay) : notDoneAllHabitsDays.push(currentDay);
+                }
+
+                setDoneAllHabitsDays(doneAllHabitsDays);
+                setNotDoneAllHabitsDays(notDoneAllHabitsDays);
+            }
+
+            const { token } = JSON.parse(localStorage.getItem("userData"));
+            const config = {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            };
+            
+            axios.get(`${BASE_URL}/habits/history/daily`, config)
+                .then(res => {
+
+                    const calendarData = res.data;
+
+                    /* This code only run when user redirect or refresh history page */
+                    if (doneAllHabitsDays === notDoneAllHabitsDays) {
+                        checkCalendarHabitStatus(calendarData);
+                    }
+                    setCalendarHabits(calendarData);
+                })
+                .catch(err => console.log(err));
+
+        } else {
+            /* Unauthorized Access or localStorage data expired */
+            navigate("/");
+        }
+
+    }, [calendar]);
+
+    if (calendarHabits === null) {
+        return (
+            <CenterLoader>
+                <Oval
+                    height="200"
+                    width="200"
+                    color="#52B6FF"
+                    ariaLabel='oval-loading'
+                    secondaryColor="#2fa8ff"
+                    strokeWidth={2}
+                    strokeWidthSecondary={2}
+                    visible={true}
+                />
+            </CenterLoader>
+        );
+    }
+
+    const presentDay = dayjs(new Date()).format('DD/MM/YYYY');
+
+    const getTileClassName = ({ date }) => {
+
+        const currentDate = dayjs(date).format('DD/MM/YYYY');
+
+        if (currentDate === presentDay) {
+            return 'react-calendar__tile--now';
+
+        } else if (doneAllHabitsDays.includes(currentDate)) {
+            return 'react-calendar__tile--all-habits-done';
+
+        } else if (notDoneAllHabitsDays.includes(currentDate)) {
+            return 'react-calendar__tile--not-all-habits-done';
+
+        } else {
+            return 'react-calendar__tile';
+        }
     }
 
     return (
@@ -21,7 +122,14 @@ export default function History() {
             <PageContent>
                 <Container>
                     <h2>Histórico</h2>
-                    <h3>Em breve você poderá ver o histórico dos seus hábitos aqui!</h3>
+                    <Calendar
+                        className='react-calendar'
+                        calendarType={'US'}
+                        onChange={setCalendar}
+                        value={calendar}
+                        tileClassName={getTileClassName}
+                        formatDay={(locale, date) => <p>{dayjs(date).format('DD')}</p>}
+                    />
                 </Container>
             </PageContent>
         </PageContainer>
--- src/pages/History/styled.js
+++ src/pages/History/styled.js
@@ -22,15 +22,123 @@ export const Container = styled.div`
 
     display: flex;
     flex-direction: column;
-    gap: 15px;
+    gap: 30px;
 
     h2 {
         font-size: 23px;
         color: #126BA5;
     }
 
-    h3 {
-        font-size: 18;
-        color: #BABABA;
+    .react-calendar {
+
+        margin: 0 auto;
+        width: 340px;
+        background-color: #FFF;
+        border-radius: 10px;
+        border: none;
+
+        &__tile {
+
+            height: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #FFF;
+            margin-top: 15px;
+            border: none;
+
+            &--now {
+
+                abbr {
+
+                    width: 35px;
+                    height: 35px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 20px;
+                    color: #000;
+                    background-color: #F9F236;
+                }
+            }
+
+            &--all-habits-done {
+
+                abbr {
+
+                    width: 35px;
+                    height: 35px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 20px;
+                    color: #000;
+                    background-color: #00C62E;
+                }
+            }
+
+            &--not-all-habits-done {
+
+                abbr {
+
+                    width: 35px;
+                    height: 35px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 20px;
+                    color: #000;
+                    background-color: #DD0037;
+                }
+            }
+
+            &--active {
+
+                background-color: #FFF;
+                color: #000;
+
+                abbr {
+
+                    width: 35px;
+                    height: 35px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 20px;
+                    background-color: #0095E5;
+                }
+
+                &:enabled {
+                    &:focus {
+
+                        background-color: #FFF;
+
+                        abbr {
+
+                            width: 35px;
+                            height: 35px;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            border-radius: 20px;
+                            background-color: #0095E5;
+                        }
+                    }
+                }
+            }
+
+            &:hover {
+                background-color: #FFF;
+                cursor: pointer;
+            }
+        }
     }
+`;
+
+export const CenterLoader = styled.div`
+
+    position: absolute;
+    top: 50%;  
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
